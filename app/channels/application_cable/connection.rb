7c89b1822e4c82e3d15cb92ecc331275416d3c79
lewis-fidlers <lewisfidlers@gmail.com>
2016-07-23 01:58:56 +0200

    Make it possible to post messages when signed in.

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -7,8 +7,9 @@ module ApplicationCable
     end
 
     protected
+
     def find_verified_user
-      if current_user = User.find_by(id: cookies.signed[:user_id])
+      if current_user = env['warden'].user
         current_user
       else
         reject_unauthorized_connection

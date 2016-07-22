9f793fd5b962de37e0c83067888bc52a4c10e4cf
lewis-fidlers <lewisfidlers@gmail.com>
2016-07-22 16:04:57 +0200

    some action cable setup.

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -4,10 +4,6 @@
 //= require action_cable
 //= require_self
 //= require_tree ./channels
-
-(function() {
-  this.App || (this.App = {});
-
-  App.cable = ActionCable.createConsumer();
-
-}).call(this);
+//
+@App ||= {}
+App.cable = ActionCable.createConsumer()
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'rooms/show'
+  get "rooms/show"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "rooms#show"
+
+  mount ActionCable.server => "/cable"
 end
